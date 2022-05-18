6ff449bd7d017e4cbeb83ebd0782c4aa0b87e564
Marga <margalppalacios@gmail.com>
2022-05-18 12:06:31 +0200

    añadir tareas

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 window.onload = function()
 {
-    var el = document.getElementById("add-task");
-    el.onclick = function()
-        {
-        console.log("click violento");
-        }
+    //var contador = 0;
+    var btn = document.getElementById("add-task");
+    var areaTexto = document.getElementById("nueva-tarea")
+    var currentUl = document.getElementById("lista-interactiva")
+    btn.onclick = function()
+    {
+        //contador++;
+        //console.log(contador);
+        var nuevoTexto = areaTexto.value;
+        //console.log(nuevoTexto);
+
+        var newLi  = document.createElement("li");
+        newLi.className="child";
+        var newContent = document.createTextNode(nuevoTexto);
+        newLi.appendChild(newContent)
+        currentUl.append(newLi)
+    }
 }
