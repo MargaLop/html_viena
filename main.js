574608db31d24da272500513c134b64e852b3b7d
Marga <margalppalacios@gmail.com>
2022-05-18 12:26:18 +0200

    bucle con js pares un color impares en otros

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 window.onload = function()
 {
-    //var contador = 0;
+    var contador = 0;
     var btn = document.getElementById("add-task");
     var areaTexto = document.getElementById("nueva-tarea")
     var currentUl = document.getElementById("lista-interactiva")
     btn.onclick = function()
     {
-        //contador++;
-        //console.log(contador);
-        var nuevoTexto = areaTexto.value;
-        //console.log(nuevoTexto);
 
+        var nuevoTexto = areaTexto.value;
         var newLi  = document.createElement("li");
         newLi.className="child";
         var newContent = document.createTextNode(nuevoTexto);
         newLi.appendChild(newContent)
         currentUl.append(newLi)
+
+        if(contador % 2 === 0)
+        {
+            newLi.className="impar";
+        }
+        else
+        {
+            newLi.className="par";
+        }
+        contador++;
     }
+
 }
